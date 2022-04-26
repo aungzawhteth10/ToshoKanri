e955a36dceee31a7d61298a92be07c9667bcb084
haedo <hd2030136@gmail.com>
2022-04-26 19:13:23 +0900

    2022/04/26

--- src/app/Api/ApiStaffRegister.php
+++ src/app/Api/ApiStaffRegister.php
@@ -10,65 +10,66 @@ class ApiStaffRegister extends ApiBase
         //スタッフ情報
         $dbStaffMapper = new \App\db\DbStaffMapper;
         $dmStaff = new \App\model\DmStaff;
-        error_log(print_r($this->session, true));
-        $dmStaff->Staff_id  = $this->session->Staff_id;
+        $dmStaff->staff_id  = 1001;//$this->session->staff_id;
         $staff = $dbStaffMapper->find($dmStaff);
+        if(count($staff) == 0){
+            return parent::toJson([]);
+        }
         $result = [
-            'staff_id'               => $staff[0]['staff_id'] , //スタッフID
-            'staff_code'             => $Staff[0]['staff_code'] ,   //利用者ID
-            'name'                   => $Staff[0]['name'] , //利用者名
-            'furigana'               => $Staff[0]['furigana'] ,    //フリガナ
-            'hyouji_ryakushou'       => $staff[0]['hyouji_ryakushou'], //表示略称
-            'seibetsu'               => $staff[0]['seibetsu'] , //性別
-            'seinenngappi_gengou'    => $staff[0]['seinenngappi_gengou'] , //生年月日元号
-            'seinenngappi_year'      => $staff[0]['seinenngappi_year'] , //生年月日元号
-            'seinenngappi_month'     => $staff[0]['seinenngappi_month'] , //生年月日元号
-            'seinenngappi_day'       => $staff[0]['seinenngappi_day'] , //生年月日元号
-            'age'                    => $staff[0]['age'] , //年齢
-            'post_no'                => $staff[0]['post_no'] , //郵便番号
-            'todoufuken'             => $staff[0]['todoufuken'] , //都道府県
-            'shikuchouson'           => $staff[0]['shikuchouson'], //市区町村
-            'chou_name'              => $staff[0]['chou_name'] , //町名
-            'banchi'                 => $staff[0]['banchi'] , //番地
-            'tel_no'                 => $staff[0]['tel_no'] ,  //電話番号
-            'moblie_no'              => $staff[0]['moblie_no'] , //携帯番号
-            'fax_no'                 => $staff[0]['fax_no'] , //fax番号
-            'mail_address'           => $staff[0]['mail_address'] , //メールアドレス
-            'bikou'                  => $staff[0]['bikou'] , //備考
+            'staff_id'               => $staff['staff_id'] , //スタッフID
+            'staff_code'             => $staff['staff_code'] ,   //利用者ID
+            'name'                   => $staff['name'] , //氏名
+            'furigana'               => $staff['furigana'] ,    //フリガナ
+            'hyouji_ryakushou'       => $staff['hyouji_ryakushou'], //表示略称
+            'seibetsu'               => $staff['seibetsu'] , //性別
+            'seinengappi_gengou'     => $staff['seinengappi_gengou'] , //生年月日元号
+            'seinengappi_year'       => $staff['seinengappi_year'] , //生年月日元号
+            'seinengappi_month'      => $staff['seinengappi_month'] , //生年月日元号
+            'seinengappi_day'        => $staff['seinengappi_day'] , //生年月日元号
+            'age'                    => $staff['age'] , //年齢
+            'post_no'                => $staff['post_no'] , //郵便番号
+            'todoufuken'             => $staff['todoufuken'] , //都道府県
+            'shikuchoson'            => $staff['shikuchoson'], //市区町村
+            'chou_name'              => $staff['chou_name'] , //町名
+            'banchi'                 => $staff['banchi'] , //番地
+            'tel_no'                 => $staff['tel_no'] ,  //電話番号
+            'moblie_no'              => $staff['moblie_no'] , //携帯番号
+            'fax_no'                 => $staff['fax_no'] , //fax番号
+            'mail_address'           => $staff['mail_address'] , //メールアドレス
+            'bikou'                  => $staff['bikou'] , //備考
         ];
         return parent::toJson($result);
     }
     /*
      * スタッフ情報更新
      */
-    public function update ($request, $response)
-    {   
-        $postData  = $_POST;    
-        $dbStaffMapper = new \App\db\DbStaffMapper;
-        $dmStaff = new \App\model\dmStaff;
-        $dmStaff->staff_id              =$postData['staff_id'];  //スタッフID
-        $dmStaff->staff_code            =$postData['staff_code'];    //スタッフコード
-        $dmStaff->eigyoushou_id         =$postData['eigyoushou_id']; //営業所ID
-        $dmStaff->name                  =$postData['name'];         //氏名
-        $dmStaff->furigana              =$postData['furigana'];         //フリガナ
-        $dmStaff->hyouji_ryakushou      =$postData['hyouji_ryakushou'];     //表示略称
-        $dmStaff->seibetsu              =$postData['seibetsu'];     //性別
-        $dmStaff->seinengappi_gengou    =$postData['seinengappi_gengou'];   //生年月日　元号
-        $dmStaff->seinengappi_year      =$postData['seinengappi_year'];     //生年月日　年
-        $dmStaff->seinengappi_month     =$postData['seinengappi_month'];    //生年月日　月
-        $dmStaff->seinengappi_day       =$postData['seinengappi_day'];      //生年月日  日
-        $dmStaff->age                   =$postData['age'];                  //年齢
-        $dmStaff->post_no               =$postData['post_no'];              //郵便番号
-        $dmStaff->todoufuken            =$postData['todoufuken'];           //都道府県  
-        $dmStaff->shikuchoson           =$postData['shikuchoson'];          //市区町村
-        $dmStaff->chou_name             =$postData['chou_name'];            //町名
-        $dmStaff->banchi                =$postData['banchi'];               //番地
-        $dmStaff->tel_no                =$postData['tel_no'];               //電話番号
-        $dmStaff->moblie_no             =$postData['moblie_no'];            //携帯番号  
-        $dmStaff->fax_no                =$postData['fax_no'];               //FAX番号
-        $dmStaff->mail_address          =$postData['mail_address'];         //メールアドレス
-        $dmStaff->bikou                 =$postData['bikou'];                //備考
-        $count = $dbStaffMapper->insert($dmStaff);
-        return parent::toJson($count);
-    }
+    // public function update ($request, $response)
+    // {   
+    //     $postData  = $_POST;    
+    //     $dbStaffMapper = new \App\db\DbStaffMapper;
+    //     $dmStaff = new \App\model\dmStaff;
+    //     $dmStaff->staff_id              =$postData['staff_id'];             //スタッフID
+    //     $dmStaff->staff_code            =$postData['staff_code'];           //スタッフコード
+    //     $dmStaff->name                  =$postData['name'];                 //氏名
+    //     $dmStaff->furigana              =$postData['furigana'];             //フリガナ
+    //     $dmStaff->hyouji_ryakushou      =$postData['hyouji_ryakushou'];     //表示略称
+    //     $dmStaff->seibetsu              =$postData['seibetsu'];             //性別
+    //     $dmStaff->seinengappi_gengou    =$postData['seinengappi_gengou'];   //生年月日　元号
+    //     $dmStaff->seinengappi_year      =$postData['seinengappi_year'];     //生年月日　年
+    //     $dmStaff->seinengappi_month     =$postData['seinengappi_month'];    //生年月日　月
+    //     $dmStaff->seinengappi_day       =$postData['seinengappi_day'];      //生年月日  日
+    //     $dmStaff->age                   =$postData['age'];                  //年齢
+    //     $dmStaff->post_no               =$postData['post_no'];              //郵便番号
+    //     $dmStaff->todoufuken            =$postData['todoufuken'];           //都道府県
+    //     $dmStaff->shikuchoson           =$postData['shikuchoson'];          //市区町村
+    //     $dmStaff->chou_name             =$postData['chou_name'];            //町名
+    //     $dmStaff->banchi                =$postData['banchi'];               //番地
+    //     $dmStaff->tel_no                =$postData['tel_no'];               //電話番号
+    //     $dmStaff->moblie_no             =$postData['moblie_no'];            //携帯番号  
+    //     $dmStaff->fax_no                =$postData['fax_no'];               //FAX番号
+    //     $dmStaff->mail_address          =$postData['mail_address'];         //メールアドレス
+    //     $dmStaff->bikou                 =$postData['bikou'];                //備考
+    //     $count = $dbStaffMapper->insert($dmStaff);
+    //     return parent::toJson($count);
+    // }
 }
